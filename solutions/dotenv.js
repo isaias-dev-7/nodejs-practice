f23b826f4e6ef7ae6201098f7b016862ca7b2eb8
isaias <isaiasosoria7@gmail.com>
2024-12-22 03:27:21 -0500

    dotenv config

---
+++ solutions/dotenv.js
@@ -0,0 +1,28 @@
+import fs from "node:fs"
+
+export function config({ path = ".env"} = {}){
+    try {
+        const env =  fs.readFileSync(path,'utf-8');
+        const lines = env.split('\n').filter((value) => value != '\r');
+
+        lines.forEach( line => {
+            const [key , ...value] = line.split('=');
+            const jValue = value.join('=');
+          
+            const valueToSave = (jValue.startsWith('"') && jValue.endsWith('"')) 
+            ? jValue.slice(1,-1) 
+            : jValue;
+            
+            process.env[key] = valueToSave;
+        });
+    } catch (error) {console.error(error.message)}
+}
+
+//support for commonjs
+const dotenv = {
+    config,
+}
+
+export default dotenv;
+
+config({});
---
+++ test/dotenv.test.js
@@ -0,0 +1,5 @@
+import { describe, it } from 'node:test';
+import { equal } from 'node:assert/strict';
+import { writeFileSync } from 'node:fs';
+import { config } from "../solutions/dotenv.js";
+
